afa730a3d1a3fb6ac01f1c5983803964a2d54b11
Lenka Erbenova <lenka.erbenova79@gmail.com>
2025-10-12 14:27:59 +0200

    Business cards application is working also with null values, styling for empty list added.

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/BusinessCardController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/BusinessCardController.java
@@ -15,9 +15,9 @@ import java.util.List;
 public class BusinessCardController {
   private final List<BusinessCard> businessCardList = List.of(
       new BusinessCard("Dita (Přikrylová) Formánková)", "Czechitas z. s.", "Václavské náměstí 837/11", "11000 Praha 1", "dita@czechitas.cz", "+420 800 123 456", "www.czechitas.cz"),
-      new BusinessCard("Barbora Bühnová", "Czechitas z. s.", "Šumavská 15", "602 00 Brno-Královo Pole", "bara@czechitas.cz", "123", "www.czechitas.cz"),
+      new BusinessCard("Barbora Bühnová", "Czechitas z. s.", "Šumavská 15", "602 00 Brno-Královo Pole", "bara@czechitas.cz", null, "www.czechitas.cz"),
       new BusinessCard("Monika Ptáčníková", "Czechitas z. s.", "Wellnerova 1215/1", "779 00 Olomouc", "monika@czechitas.cz", "+420 585 111 111", "www.czechitas.cz"),
-      new BusinessCard("Mirka Zatloukalová", "Czechitas z. s.", "Václavské náměstí 837/11", "11000 Praha 1", "mirka@czechitas.cz", "+420 800 123 456", "www.czechitas.cz")
+      new BusinessCard("Mirka Zatloukalová", "Czechitas z. s.", "Václavské náměstí 837/11", "11000 Praha 1", null, "+420 800 123 456", "www.czechitas.cz")
   );
 
   @GetMapping("/") //main route
