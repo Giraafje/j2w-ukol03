bbf6baae641ed8be545908d32a170d2933f51fec
Lenka Erbenova <lenka.erbenova79@gmail.com>
2025-10-17 11:25:39 +0200

    Create a class BusinessCardService, adjust BusinessCardController to use the service, small change in the dynamic route.

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/BusinessCardController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/BusinessCardController.java
@@ -1,36 +1,34 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
-import cz.czechitas.java2webapps.ukol3.entities.BusinessCard;
+import cz.czechitas.java2webapps.ukol3.service.BusinessCardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 /**
  * Kontroler obsluhující zobrazování vizitek.
  */
 @Controller
 public class BusinessCardController {
-  private final List<BusinessCard> businessCardList = List.of(
-      new BusinessCard("Dita (Přikrylová) Formánková)", "Czechitas z. s.", "Václavské náměstí 837/11", "11000 Praha 1", "dita@czechitas.cz", "+420 800 123 456", "www.czechitas.cz"),
-      new BusinessCard("Barbora Bühnová", "Czechitas z. s.", "Šumavská 15", "602 00 Brno-Královo Pole", "bara@czechitas.cz", null, "www.czechitas.cz"),
-      new BusinessCard("Monika Ptáčníková", "Czechitas z. s.", "Wellnerova 1215/1", "779 00 Olomouc", "monika@czechitas.cz", "+420 585 111 111", "www.czechitas.cz"),
-      new BusinessCard("Mirka Zatloukalová", "Czechitas z. s.", "Václavské náměstí 837/11", "11000 Praha 1", null, "+420 800 123 456", "www.czechitas.cz")
-  );
+  private final BusinessCardService service;
+
+  //inversion of control, connection with the service
+  public BusinessCardController(BusinessCardService service) {
+    this.service = service;
+  }
 
   @GetMapping("/") //main route
   public ModelAndView seznam() {
     ModelAndView modelAndView = new ModelAndView("seznam");
-    modelAndView.addObject("businessCardList", businessCardList);
+    modelAndView.addObject("businessCardList", service.getAll());
     return modelAndView;
   }
 
-  @GetMapping("/{id}") //dynamic route for each card detail
+  @GetMapping("/detail/{id}") //dynamic route for each card detail
   public ModelAndView detail(@PathVariable int id) {
     ModelAndView modelAndView = new ModelAndView("detail");
-    modelAndView.addObject("businessCard", businessCardList.get(id));
+    modelAndView.addObject("businessCard", service.getById(id));
     return modelAndView;
   }
 }
---
+++ src/main/java/cz/czechitas/java2webapps/ukol3/service/BusinessCardService.java
@@ -0,0 +1,28 @@
+package cz.czechitas.java2webapps.ukol3.service;
+
+import cz.czechitas.java2webapps.ukol3.entities.BusinessCard;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BusinessCardService {
+  private final List<BusinessCard> businessCardList = new ArrayList<>();
+
+  // constructor
+  public BusinessCardService() {
+    businessCardList.add(new BusinessCard("Dita (Přikrylová) Formánková)", "Czechitas z. s.", "Václavské náměstí 837/11", "11000 Praha 1", "dita@czechitas.cz", "+420 800 123 456", "www.czechitas.cz"));
+    businessCardList.add(new BusinessCard("Barbora Bühnová", "Czechitas z. s.", "Šumavská 15", "602 00 Brno-Královo Pole", "bara@czechitas.cz", null, "www.czechitas.cz"));
+    businessCardList.add(new BusinessCard("Monika Ptáčníková", "Czechitas z. s.", "Wellnerova 1215/1", "779 00 Olomouc", "monika@czechitas.cz", "+420 585 111 111", "www.czechitas.cz"));
+    businessCardList.add(new BusinessCard("Mirka Zatloukalová", "Czechitas z. s.", "Václavské náměstí 837/11", "11000 Praha 1", null, "+420 800 123 456", "www.czechitas.cz"));
+  }
+
+  public List<BusinessCard> getAll() {
+    return businessCardList;
+  }
+
+  public BusinessCard getById(int id) {
+    return businessCardList.get(id);
+  }
+}
